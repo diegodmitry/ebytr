4ce0c33f3bc8d098e80b79a7ff0938bffefbcd10
Diego Dmitry <diegodmitry@hotmail.com>
2022-07-04 17:02:33 -0300

    feat: added context.

---
+++ frontend/src/context/Context.js
@@ -0,0 +1,40 @@
+import React, { createContext, useEffect, useState } from 'react';
+import fetchTasksAPI from '../services/fetchAPI';
+
+export const GlobalContext = createContext();
+function TasksProvider({ children }) {
+  const [originalDatas, setOriginalDatas] = useState([]);
+  const [datas, setDatas] = useState([]);
+  console.log("🚀 ~ file: Context.js ~ line 7 ~ TasksProvider ~ datas", datas);
+
+  const editedDatas = async (tasks) => {
+    const newDatas = tasks.map((data) => (
+      {id: data.id, taskName: data.taskName, statusTask: data.statusTask, createAt: data.createAt.slice(0, 10)}
+    ));
+    return newDatas;
+  };
+
+  async function fetchTasks() {
+    const tasks = await fetchTasksAPI();
+    const newDatas = await editedDatas(tasks);
+    setDatas(newDatas);
+    setOriginalDatas(tasks);
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const contextValue = {
+    originalDatas,
+    datas,
+  }
+
+  return (
+    <GlobalContext.Provider value={ contextValue }>
+      { children }
+    </GlobalContext.Provider>
+  );
+}
+
+export default TasksProvider;
